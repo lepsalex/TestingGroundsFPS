c9617fad21229f83ced66d6989fb8bd4b434cb4a
Alex <lepsalex@gmail.com>
2018-12-02 19:04:47 -0500

    Blackboard access in C++

--- Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
+++ Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-    UE_LOG(LogTemp, Warning, TEXT("UChooseNextWaypoint Executing!"));
+    auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+    auto IndexValue = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    UE_LOG(LogTemp, Warning, TEXT("Current Index: %i"), IndexValue);
     return EBTNodeResult::Succeeded;
 }
