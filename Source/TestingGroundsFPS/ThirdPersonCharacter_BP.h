189f97d262df6d3503d5e057275a3620f44d85d5
Alex <lepsalex@gmail.com>
2018-12-02 19:20:56 -0500

    New base class for TPC

---
+++ Source/TestingGroundsFPS/ThirdPersonCharacter_BP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "ThirdPersonCharacter_BP.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TESTINGGROUNDSFPS_API AThirdPersonCharacter_BP : public ATP_ThirdPersonCharacter {
+    GENERATED_BODY()
+
+  private:
+    UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+    TArray<AActor*> PatrolPoints;
+};
