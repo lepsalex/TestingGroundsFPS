7bf1c53ed755875b9fd536858a31dd6b4cfd6859
Alex <lepsalex@gmail.com>
2018-12-05 21:39:26 -0500

    C++ Reorg/ReFactor

--- Source/TestingGroundsFPS/TestingGroundsFPSGameMode.cpp
+++ Source/TestingGroundsFPS/TestingGroundsFPSGameMode.cpp
@@ -2,16 +2,15 @@
 
 #include "TestingGroundsFPSGameMode.h"
 #include "TestingGroundsFPSHUD.h"
-#include "TestingGroundsFPSCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATestingGroundsFPSGameMode::ATestingGroundsFPSGameMode()
-	: Super()
-{
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+    : Super() {
+    // set default pawn class to our Blueprinted character
+    static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+    DefaultPawnClass = PlayerPawnClassFinder.Class;
 
-	// use our custom HUD class
-	HUDClass = ATestingGroundsFPSHUD::StaticClass();
+    // use our custom HUD class
+    HUDClass = ATestingGroundsFPSHUD::StaticClass();
 }
